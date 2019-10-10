bd32031bee78a5899e99e6770cbdea062a6e2a58
Óscar Vega <s16120245@alumnos.itsur.edu.mx>
2019-10-09 20:35:58 -0500

    Cambio al Main

--- app/src/main/java/com/vegamex/audiolibrosbasico/MainActivity.java
+++ app/src/main/java/com/vegamex/audiolibrosbasico/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 import com.vegamex.audiolibrosbasico.fragments.DetalleFragment;
 import com.vegamex.audiolibrosbasico.fragments.SelectorFragment;
@@ -25,7 +24,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
@@ -39,20 +38,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if ((findViewById(R.id.contenedor_pequeno) != null) &&
-                (getSupportFragmentManager().findFragmentById(
-                        R.id.contenedor_pequeno) == null)){
-            SelectorFragment primerFragment = new SelectorFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.contenedor_pequeno, primerFragment).commit();
-        }
-
-
-        //Barra de acciones
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //Botón flotante
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,11 +51,20 @@ public class MainActivity extends AppCompatActivity {
 
         Aplicacion app = (Aplicacion) getApplication();
 
+
+        if ((findViewById(R.id.contenedor_pequeno) != null) &&
+                (getSupportFragmentManager().findFragmentById(
+                        R.id.contenedor_pequeno) == null)){
+            SelectorFragment primerFragment = new SelectorFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contenedor_pequeno, primerFragment).commit();
+        }
+
         //Pestañas
-        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.addTab(tabs.newTab().setText("Todos"));
         tabs.addTab(tabs.newTab().setText("Nuevos"));
-        tabs.addTab(tabs.newTab().setText("Leídos"));
+        tabs.addTab(tabs.newTab().setText("Leidos"));
         tabs.setTabMode(TabLayout.MODE_SCROLLABLE);
         tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override public void onTabSelected(TabLayout.Tab tab) {
@@ -80,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
                         adaptador.setNovedad(true);
                         adaptador.setLeido(false);
                         break;
-                    case 2: //Leídos
+                    case 2: //Leidos
                         adaptador.setNovedad(false);
                         adaptador.setLeido(true);
                         break;
@@ -93,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
         adaptador = ((Aplicacion) getApplicationContext()).getAdaptador();
 
+
         // Navigation Drawer
         drawer = (DrawerLayout) findViewById(
                 R.id.drawer_layout);
@@ -102,74 +100,14 @@ public class MainActivity extends AppCompatActivity {
         toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(
                 R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+        navigationView.setNavigationItemSelectedListener(this);
 
         appBarLayout = (AppBarLayout) findViewById(R.id.appBarLayout);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.menu_preferencias) {
-            Toast.makeText(this, "Preferencias", Toast.LENGTH_LONG).show();
-            return true;
-        } else if (id == R.id.menu_ultimo) {
-            irUltimoVisitado();
-            return true;
-        } else if (id == R.id.menu_buscar) {
-            return true;
-        } else if (id == R.id.menu_acerca) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setMessage("Mensaje de Acerca De");
-            builder.setPositiveButton(android.R.string.ok, null);
-            builder.create().show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void mostrarDetalle(int id) {
-        DetalleFragment detalleFragment = (DetalleFragment)
-                getSupportFragmentManager().findFragmentById(R.id.detalle_fragment);
-        if (detalleFragment != null) {
-            detalleFragment.ponInfoLibro(id);
-        } else {
-            DetalleFragment nuevoFragment = new DetalleFragment();
-            Bundle args = new Bundle();
-            args.putInt(DetalleFragment.ARG_ID_LIBRO, id);
-            nuevoFragment.setArguments(args);
-            FragmentTransaction transaccion = getSupportFragmentManager()
-                    .beginTransaction();
-            transaccion.replace(R.id.contenedor_pequeno, nuevoFragment);
-            transaccion.addToBackStack(null);
-            transaccion.commit();
-        }
-        SharedPreferences pref = getSharedPreferences(
-                "com.example.audiolibros_internal", MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
-        editor.putInt("ultimo", id);
-        editor.commit();
-    }
-
-    public void irUltimoVisitado() {
-        SharedPreferences pref = getSharedPreferences(
-                "com.example.audiolibros_internal", MODE_PRIVATE);
-        int id = pref.getInt("ultimo", -1);
-        if (id >= 0) {
-            mostrarDetalle(id);
-        } else {
-            Toast.makeText(this,"Sin última vista",Toast.LENGTH_LONG).show();
-        }
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
-    //@Override
+    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.nav_todos) {
@@ -202,6 +140,66 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void mostrarDetalle(int id) {
+        DetalleFragment detalleFragment = (DetalleFragment)
+                getSupportFragmentManager().findFragmentById(R.id.detalle_fragment);
+        if (detalleFragment != null) {
+            detalleFragment.ponInfoLibro(id);
+        } else {
+            DetalleFragment nuevoFragment = new DetalleFragment();
+            Bundle args = new Bundle();
+            args.putInt(DetalleFragment.ARG_ID_LIBRO, id);
+            nuevoFragment.setArguments(args);
+            FragmentTransaction transaccion = getSupportFragmentManager()
+                    .beginTransaction();
+            transaccion.replace(R.id.contenedor_pequeno, nuevoFragment);
+            transaccion.addToBackStack(null);
+            transaccion.commit();
+        }
+        SharedPreferences pref = getSharedPreferences(
+                "com.example.audiolibros_internal", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("ultimo", id);
+        editor.commit();
+    }
+
+
+    public void irUltimoVisitado() {
+        SharedPreferences pref = getSharedPreferences(
+                "com.example.audiolibros_internal", MODE_PRIVATE);
+        int id = pref.getInt("ultimo", -1);
+        if (id >= 0) {
+            mostrarDetalle(id);
+        } else {
+            Toast.makeText(this,"Sin última vista",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_preferencias) {
+            Toast.makeText(this, "Preferencias", Toast.LENGTH_LONG).show();
+            return true;
+        } else if (id == R.id.menu_ultimo) {
+            irUltimoVisitado();
+            return true;
+        } else if (id == R.id.menu_buscar) {
+            return true;
+        } else if (id == R.id.menu_acerca) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Mensaje de Acerca De");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.create().show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void mostrarElementos(boolean mostrar) {
         appBarLayout.setExpanded(mostrar);
         toggle.setDrawerIndicatorEnabled(mostrar);
@@ -213,5 +211,4 @@ public class MainActivity extends AppCompatActivity {
             drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
         }
     }
-
 }
